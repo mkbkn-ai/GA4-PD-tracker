0188be8108d3e9d54f2e4c921e4350ebaaa85849
mkbkn-ai <mikebekin@gmail.com>
2026-05-19 16:24:03 +0100

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,1 +1,18 @@
+import requests
+import os
 
+PIPEDRIVE_API_TOKEN = os.getenv('PIPEDRIVE_API_TOKEN')
+PIPEDRIVE_API_URL = "https://api.pipedrive.com/v1/persons"
+
+def send_to_pipedrive(data):
+    payload = {
+        "name": data.get('name'),
+        "email": data.get('email'),
+        "visible_to": 3,
+        "utm_source": data.get('utm_source'),
+        "utm_medium": data.get('utm_medium'),
+        "utm_campaign": data.get('utm_campaign'),
+        "gclid": data.get('gclid')
+    }
+    response = requests.post(f"{PIPEDRIVE_API_URL}?api_token={PIPEDRIVE_API_TOKEN}", json=payload)
+    return response.status_code == 201
